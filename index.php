d74cb1a2d3451b832ef2acbbf60158bc50859570
VeerMuchandi <veer.muchandi@gmail.com>
2015-07-26 23:04:05 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// Create a 55x30 image
+$im = imagecreatetruecolor(55, 30);
+$white = imagecolorallocate($im, 255, 255, 255);
+
+// Draw a white rectangle
+imagefilledrectangle($im, 4, 4, 50, 25, $white);
+
+// Save the image
+imagepng($im, './imagefilledrectangle.png');
+imagedestroy($im);
+?>
