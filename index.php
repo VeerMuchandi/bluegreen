cb7c225af8f8a3deb9e3c6baa0ce629e4e8b9939
VeerMuchandi <veer.muchandi@gmail.com>
2015-07-28 00:24:29 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ $blue = imagecolorallocate($im, 0, 0, 255);
 $green = imagecolorallocate($im, 0, 255, 0);
 
 // Draw a white rectangle
-//imagefilledrectangle($im, 4, 4, 192, 92, $blue);
-imagefilledrectangle($im, 4, 4, 192, 92, $green);
+//imagefilledrectangle($im, 4, 4, 196, 96, $blue);
+imagefilledrectangle($im, 4, 4, 196, 96, $green);
 
 // Save the image
 header('Content-Type: image/png');
