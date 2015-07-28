7597e65e203f4a72ecc6ddb853dbb0cb6878bce2
VeerMuchandi <veer.muchandi@gmail.com>
2015-07-27 22:47:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ Welcome!!
  $white = ImageColorAllocate($im,0xFF,0xFF,0xFF);
  $black = ImageColorAllocate($im,0x00,0x00,0x00);
  ImageFilledRectangle($im,50,50,150,150,$black);
- header('Content-Type: image/png');
- ImagePNG($im);
+ header('Content-Type: image/jpeg');
+ ImageJPEG($im);
 ?>
