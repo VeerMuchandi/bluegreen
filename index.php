507b6c5c2fccfd221dca06252e14870fd5095d7d
VeerMuchandi <veer.muchandi@gmail.com>
2015-07-27 23:28:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,13 @@ Welcome!!
 <?php
 // Create a 55x30 image
 $im = imagecreatetruecolor(55, 30);
-$white = imagecolorallocate($im, 0, 0, 255);
+$white = imagecolorallocate($im, 255, 255, 255);
+$blue = imagecolorallocate($im, 0, 0, 255);
+$green = imagecolorallocate($im, 0, 255, 0);
 
 // Draw a white rectangle
-imagefilledrectangle($im, 4, 4, 50, 25, $white);
+//imagefilledrectangle($im, 4, 4, 50, 25, $blue);
+imagefilledrectangle($im, 4, 4, 50, 25, $green);
 
 // Save the image
 header('Content-Type: image/png');
