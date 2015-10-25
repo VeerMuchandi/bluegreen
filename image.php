512bf3d04e9309ff35763ce34b6027bcfbf86327
VeerMuchandi <veer.muchandi@gmail.com>
2015-10-25 18:55:31 -0400

    Update image.php

--- image.php
+++ image.php
@@ -5,9 +5,9 @@ $white = imagecolorallocate($im, 255, 255, 255);
 $blue = imagecolorallocate($im, 0, 0, 255);
 $green = imagecolorallocate($im, 0, 255, 0);
 
-// Draw a white rectangle
-imagefilledrectangle($im, 0, 0, 199, 199, $blue);
-//imagefilledrectangle($im, 0, 0, 199, 199, $green);
+// Draw a filled rectangle
+//imagefilledrectangle($im, 0, 0, 199, 199, $blue);
+imagefilledrectangle($im, 0, 0, 199, 199, $green);
 
 // Save the image
 header('Content-Type: image/png');
